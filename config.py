9ad4cb6b15c8d0c3296704a67f88c670cf9a4e2b
Vlad <v.reskajs@gmail.com>
2025-05-17 19:11:41 +0300

    fix: remove dotenv usage

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+if os.getenv("RAILWAY_ENVIRONMENT_ID") is None:
+    from dotenv import load_dotenv
+    load_dotenv()
 
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    DB_URL = os.getenv("DB_URL", "sqlite+aiosqlite:///./finance.db")
+    DB_URL = os.getenv("DB_URL")

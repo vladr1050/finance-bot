baa6a1ffad8c95e7e926fdb9863308318bbeeaf4
Vlad <v.reskajs@gmail.com>
2025-05-24 10:33:07 +0300

    🐛 Fix missing BigInteger import

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey, JSON
+from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime, ForeignKey, JSON, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from datetime import datetime

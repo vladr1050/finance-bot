02cb98696bd1e095853db8166653318a60e72054
Vlad <v.reskajs@gmail.com>
2025-05-29 07:09:28 +0300

    🐛 Fix: correct forecast_router construction to avoid double attach

--- app/bot/handlers/forecast/__init__.py
+++ app/bot/handlers/forecast/__init__.py
@@ -1,11 +1,12 @@
+from aiogram import Router
+
 from .entry import entry_router
 from .save import save_router
 from .view import view_router
 from .help import help_router
 
-forecast_router = (
-    entry_router
-    .include_router(save_router)
-    .include_router(view_router)
-    .include_router(help_router)
-)
+forecast_router = Router()
+forecast_router.include_router(entry_router)
+forecast_router.include_router(save_router)
+forecast_router.include_router(view_router)
+forecast_router.include_router(help_router)
